2d86e98b9818b5ad569c8aa35f11b0dd6ac55cdb
Dara-Leonard Jenssen Etemady <dletemad@ulrik.uio.no>
2024-04-15 03:34:12 +0200

    R analysis

---
+++ corpus_analysis.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+data <- read.csv("nowac-raw-tokens-consonant-clusters.csv")
+head(data)
+data <- data %>% 
+  rename(onset = X)
+  arrange(desc(frequency)) %>% 
+  mutate(logf = log10(frequency)) %>% 
+  mutate(rank = row_number()) %>%
+  mutate(logr = log10(rank)) %>%
+  mutate(zipf = log10((frequency/sum(frequency))*1000000000)) %>%
+  mutate(length = nchar(X))
+data
+ggplot(data, aes(x = logr, y = zipf)) +
+  geom_point()
+
+zipf7 <- which(data$zipf >= 7)
+data$onset[zipf7]
+zipf6 <- which(data$zipf >= 6 & data$zipf < 7)
+data$onset[zipf6]
+zipf5 <- which(data$zipf >= 5 & data$zipf < 6)
+data$onset[zipf5]
+zipf4 <- which(data$zipf >= 4 & data$zipf < 5)
+data$onset[zipf4]
+zipf3 <- which(data$zipf >= 3 & data$zipf < 4)
+data$onset[zipf3]
+
+data$zipf[which(data$onset
+                == "sn")]
